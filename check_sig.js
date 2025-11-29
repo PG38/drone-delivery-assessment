249d3c7a87e01c8b27f273127a98f5cfde69d2e5
PG38 <109286942+PG38@users.noreply.github.com>
2025-11-29 15:12:07 +0300

    Cleanup: Remove dev scripts and unused dependencies

--- check_sig.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-const buffer = fs.readFileSync('requirements.pdf');
-console.log('First 4 bytes:', buffer.subarray(0, 4).toString('hex'));
-console.log('First 4 chars:', buffer.subarray(0, 4).toString('utf8'));
--- read_pdf.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-
-reader = PdfReader("requirements.pdf")
-text = ""
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-
-print(text)
